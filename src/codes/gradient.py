3601783b4caf6bd89c30b73ffcf46b83ba46ef49
Tej <tej.narayan@gmail.com>
2022-08-15 14:33:11 +0530

    Add files via upload

---
+++ src/codes/gradient.py
@@ -0,0 +1,13 @@
+#Gradient Descent
+new_x = 3
+previous_x = 0
+step_multiplier = 0.1
+
+for n in range(30):
+    previous_x = new_x
+    gradient = df(previous_x)
+    new_x = previous_x - step_multiplier * gradient
+    
+print('Local minimum occurs at:', new_x)
+print('Slope or df(x) value at this point is:', df(new_x))
+print('f(x) value or cost at this point is:', f(new_x))
