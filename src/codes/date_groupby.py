faf773e61496cd0a9e8785588d5a3e660a7c301c
Tej <tej.narayan@gmail.com>
2022-08-15 14:32:27 +0530

    Add files via upload

---
+++ src/codes/date_groupby.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+df = pd.DataFrame({"date":["3-15-22", "3-16-22","3-22-22"],
+                   "price": [2,3,4]})
+
+df["date"] = pd.to_datetime(df["date"])
+print(df)
+
+df1 = df.groupby(pd.Grouper(key="date",freq="1W")).mean()
+print(df1)
