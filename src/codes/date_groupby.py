1b2d16c64fe89876a888f90e80520718e339b482
Tej <tej.narayan@gmail.com>
2026-01-05 14:44:42 +0530

    Update date_groupby.py

--- src/codes/date_groupby.py
+++ src/codes/date_groupby.py
@@ -1,4 +1,4 @@
-#Import library
+# Import library
 import pandas as pd
 
 df = pd.DataFrame({"date":["3-15-22", "3-16-22","3-22-22"],
@@ -9,4 +9,5 @@ print(df)
 
 df1 = df.groupby(pd.Grouper(key="date",freq="1W")).mean()
 print(df1)
+
 
